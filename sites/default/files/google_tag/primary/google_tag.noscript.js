1da106b79d771def831a707a9beb87ee6c8c9356
Joshua Fernandes <joshua@sjinnovation.com>
2019-04-13 11:34:41 +0530

    added fixes

--- sites/default/files/google_tag/primary/google_tag.noscript.js
+++ sites/default/files/google_tag/primary/google_tag.noscript.js
@@ -1,1 +1,1 @@
-<noscript aria-hidden="true"><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LZVGG7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript aria-hidden="true"><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LZVGG7" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
